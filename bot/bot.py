a8f686adb7e5afed89a93a7cc36d76261d39cca0
Aleksandr <aleksandr_perelygin@epam.com>
2019-11-27 14:47:04 +0300

    fix

--- bot/bot.py
+++ bot/bot.py
@@ -13,11 +13,6 @@ bot = telebot.TeleBot(TOKEN)
 
 # / memory -> dict / ram -> dict / cpy -> get_pid -> dict
 memory = memory_usage()["Memory"]
-print(memory)
-# memory["total"]
-# memory["used"]
-# memory['free']
-# memory['%']
 
 
 @bot.message_handler(commands=['start'])
@@ -28,25 +23,27 @@ def start_message(message):
 @bot.message_handler(commands=["ram", "memory", "cpu"])
 def send_info(message):
     if message.text == "/ram":
-        bot.send_message(message.chat.id, "Not Works", reply_markup=clear_markup)
+        bot.send_message(message.chat.id, "Not Works now")
     elif message.text == "/memory":
         bot.send_message(message.chat.id, "___________MEMORY______________"
                                           f"Total   : f{memory['total']}\n"
                                           f"Used    : f{memory['used']}\n"
                                           f"Free    : f{memory['free']}\n"
-                                          f"Percent : f{memory['%']}\n", reply_markup=clear_markup)
+                                          f"Percent : f{memory['%']}\n")
     elif message.text == "/cpu":
         bot.send_message(message.chat.id, "IT's not worked")
     else:
-        bot.send_message(message.chat.id, "You can saw only: ram or memory info", reply_markup=keyboard_main)
+        bot.send_message(message.chat.id, "You can saw only: ram or memory info")
 
 
 clear_markup = types.ReplyKeyboardRemove(selective=False)
-keyboard_main = telebot.types.InlineKeyboardMarkup()
-memory_btn = types.InlineKeyboardButton("/memory")
-ram_btn = types.InlineKeyboardButton("/ram")
-cpy_btn = types.InlineKeyboardButton("/cpy")
-keyboard_main.add(memory_btn, ram_btn, cpy_btn)
 
+
+memory_btn = types.InlineKeyboardButton("/memory", callback_data="/memory")
+ram_btn = types.InlineKeyboardButton("/ram", callback_data="/ram")
+cpy_btn = types.InlineKeyboardButton("/cpy", callback_data="/cpy")
+
+custom_keyboard = [[memory_btn, ram_btn, cpy_btn]]
+keyboard_main = telebot.types.ReplyKeyboardMarkup(custom_keyboard)
 
 bot.polling(none_stop=False, interval=0.5, timeout=0)
