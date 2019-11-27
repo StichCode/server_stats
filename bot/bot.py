da4ba8bb6dde5e565163c0ee9121e16c71e8fc46
Alex <rabbit_1399@icloud.com>
2019-11-27 19:50:11 +0000

    Dev

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ import os
 import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+from bot.db.database import create_db, has_user_permission, create_new_user, get_users_to_permissions
 from bot.get_info import memory_usage, prepare_data
 
 TOKEN = os.environ.get("TOKEN")
@@ -18,36 +19,37 @@ emoji = u"\u2744"
 def start_message(message):
     bot.send_message(message.chat.id, "Hi, what you want to known?\n"
                                       "I can show you info about:\n"
-                                      f"RAM    {emoji}\n"
-                                      f"MEMORY {emoji}\n"
-                                      f"CPU    {emoji}", reply_markup=reply_markup)
-
-
-@bot.message_handler(commands=["ram", "memory", "cpu"])
-def send_info(message):
-    if message.text == "/ram":
-        bot.send_message(message.chat.id, prepare_data())
-    elif message.text == "/memory":
-        bot.send_message(message.chat.id, "___________MEMORY______________\n"
-                                          f"Total   : {round(memory['total'], 3)} GiB\n"
-                                          f"Used    : {round(memory['used'], 3)} GiB\n"
-                                          f"Free    : {round(memory['free'], 3)} GiB\n"
-                                          f"Percent : {round(memory['%'], 3)} %\n")
-    elif message.text == "/cpu":
-        bot.send_message(message.chat.id, "IT's not worked")
-    else:
-        bot.send_message(message.chat.id, "You can saw only: ram or memory info")
+                                      f"RAM      {emoji}\n"
+                                      f"MEMORY   {emoji}\n"
+                                      f"CPU      {emoji}", reply_markup=reply_markup)
 
 
 @bot.message_handler(content_types=["text"])
 def send_info(message):
-    if message.text == "/check":
-        pass
+    perm = has_user_permission(message.from_user.id)
+    if perm:
+        if message.text == "/ram":
+            bot.send_message(message.chat.id, prepare_data())
+        elif message.text == "/memory":
+            bot.send_message(message.chat.id, "___________MEMORY______________\n"
+                                              f"Total   : {round(memory['total'], 3)} GiB\n"
+                                              f"Used    : {round(memory['used'], 3)} GiB\n"
+                                              f"Free    : {round(memory['free'], 3)} GiB\n"
+                                              f"Percent : {round(memory['%'], 3)} %\n")
+        elif message.text == "/cpu":
+            bot.send_message(message.chat.id, "IT's not worked")
+        elif message.text == "/want_permissions":
+            bot.send_message(message.chat.id, get_users_to_permissions())
+
+        else:
+            bot.send_message(message.chat.id, "I can show you only:\n"
+                                              f"RAM    {emoji}\n"
+                                              f"MEMORY {emoji}\n"
+                                              f"CPU    {emoji}")
+
     else:
-        bot.send_message(message.chat.id, "I can show you only:\n"
-                                          f"RAM    {emoji}\n"
-                                          f"MEMORY {emoji}\n"
-                                          f"CPU    {emoji}")
+        bot.send_message(message.chat.id, "You don't have permission to use bot\n")
+        create_new_user(message.from_user.id, message.from_user.username)
 
 
 memory_btn = InlineKeyboardButton(text="/memory", callback_data="memory")
@@ -58,4 +60,5 @@ custom_keyboard = [[memory_btn, ram_btn, cpy_btn]]
 reply_markup = InlineKeyboardMarkup(custom_keyboard)
 
 
+create_db()
 bot.polling(none_stop=False, interval=0.5, timeout=0)
---
+++ bot/db/database.py
@@ -0,0 +1,87 @@
+import sqlite3
+
+
+def create_db():
+    """ Create database """
+    conn = __connection()
+    cursor = conn.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY, username TEXT, "
+                   "sign_in BOOLEAN, admin BOOLEAN)")
+    create_new_user(295290188, 'rabbit_666', True, True)
+    conn.commit()
+    conn.close()
+
+
+def __connection():
+    """ Connection to database """
+    return sqlite3.connect("database_status.db", check_same_thread=True)
+
+
+def get_all_data():
+    """ Fetch all data from database"""
+    conn = __connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    conn.close()
+    return users
+
+
+def create_new_user(id_user, username, has_permission=False, admin=False):
+    """Crate new user """
+    conn = __connection()
+    flag = False
+    cursor = conn.cursor()
+    try:
+        cursor.execute("INSERT INTO users (id, username, sign_in, admin) "
+                       "VALUES (?, ?, ?, ?)", (id_user, username, has_permission, admin))
+    except sqlite3.IntegrityError:
+        flag = False
+    conn.commit()
+    conn.close()
+    return flag
+
+
+def has_user_permission(id_users):
+    """ Get user by id """
+    conn = __connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT sign_in FROM users where id = {}".format(id_users))
+    has_permission = cursor.fetchone()
+    conn.close()
+    if has_permission is None or has_permission[0] == 0:
+        return False
+    return True
+
+
+def edit_user_settings(id_user, id_to_change):
+    """Gives user login permission"""
+    conn = __connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT admin FROM users where id = {}".format(id_user))
+    if cursor.fetchone()[0] == 1:
+        if cursor.execute("SELECT sign_in FROM users").fetchone()[0] == 1:
+            return "User can already use the functional of the bot"
+        cursor.execute("UPDATE users SET sign_in = {} WHERE id = {}".format(True, id_to_change))
+        conn.commit()
+        return "The changes are completed, user can use the functional of the bot"
+    else:
+        return "You don't have permission to do this"
+
+
+def get_users_to_permissions():
+    conn = __connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users where sign_in = 0")
+    users = cursor.fetchall()
+    conn.close()
+    if not users:
+        return "No users what want to have permission"
+    text = "___This users want permissions___\n"
+    count = 1
+    for i in range(len(users)):
+        text += f"{count} - Name: {users[i][1]} Id: {users[i][0]}\n"
+        text += "__________________________________\n"
+        count += 1
+    return text
